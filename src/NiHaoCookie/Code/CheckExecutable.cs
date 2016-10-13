1f7d4f984655abb76c6ea6e5a56a8dccc60d11c6
Administrator <quandary82@hailmail.net>
2016-10-13 12:20:41 +0200

    Added iptables-management

---
+++ src/NiHaoCookie/Code/CheckExecutable.cs
@@ -0,0 +1,59 @@
+﻿
+namespace NiHaoCookie
+{
+
+
+    public class ExecutableExists 
+    {
+
+        private static bool? m_IpTablesExists;
+        public static bool IPtables
+        {
+            get
+            {
+                if (m_IpTablesExists.HasValue)
+                    return m_IpTablesExists.Value;
+
+                m_IpTablesExists = ExistsOnPath("iptables");
+                return m_IpTablesExists.Value;
+            }
+        }
+
+        private static bool? m_ServiceExists;
+        public static bool Service
+        {
+            get
+            {
+                if (m_ServiceExists.HasValue)
+                    return m_ServiceExists.Value;
+
+                m_ServiceExists = ExistsOnPath("service");
+                return m_ServiceExists.Value;
+            }
+        }
+
+
+        public static bool ExistsOnPath(string fileName)
+        {
+            return GetFullPath(fileName) != null;
+        }
+
+        public static string GetFullPath(string fileName)
+        {
+            if (System.IO.File.Exists(fileName))
+                return System.IO.Path.GetFullPath(fileName);
+
+            var values = System.Environment.GetEnvironmentVariable("PATH");
+            foreach (var path in values.Split(';'))
+            {
+                var fullPath = System.IO.Path.Combine(path, fileName);
+                if (System.IO.File.Exists(fullPath))
+                    return fullPath;
+            }
+            return null;
+        }
+
+    }
+
+
+}
---
+++ src/NiHaoCookie/Code/IPtables.cs
@@ -0,0 +1,66 @@
+﻿
+namespace NiHaoCookie
+{
+
+
+    public class IPtables
+    {
+
+
+        public void Ban(string IP)
+        {
+            if (!ExecutableExists.IPtables)
+                return;
+
+            System.Net.IPAddress ipa;
+            if (System.Net.IPAddress.TryParse(IP, out ipa))
+            {
+                // One port only:  iptables -A INPUT -s 65.55.44.100 -p tcp --destination-port 25 -j DROP
+                // All ports: iptables -A INPUT -s 65.55.44.100 -j DROP
+                using (System.Diagnostics.Process proc = System.Diagnostics.Process.Start("iptables", "-A INPUT -s " + IP + " -j DROP"))
+                {
+                    proc.WaitForExit();
+                } // End Using proc 
+
+                SaveConfig();
+            } // End if (System.Net.IPAddress.TryParse(IP, out ipa)) 
+
+        } // End Sub Ban 
+
+
+        public void Unban(string IP)
+        {
+            if (!ExecutableExists.IPtables)
+                return;
+
+            System.Net.IPAddress ipa;
+            if (System.Net.IPAddress.TryParse(IP, out ipa))
+            {
+                // iptables -D INPUT -s 66.55.44.33 -j DROP
+                using (System.Diagnostics.Process proc = System.Diagnostics.Process.Start("iptables", "-D INPUT -s " + IP + " -j DROP"))
+                {
+                    proc.WaitForExit();
+                } // End Using proc 
+
+                SaveConfig();
+            } // End if (System.Net.IPAddress.TryParse(IP, out ipa)) 
+
+        } // End Sub Unban 
+
+
+        private void SaveConfig()
+        {
+            if (!ExecutableExists.IPtables || !ExecutableExists.Service)
+                return;
+
+            using (System.Diagnostics.Process proc = System.Diagnostics.Process.Start("service", "iptables save"))
+            {
+                proc.WaitForExit();
+            } // End Using proc 
+        } // End Sub SaveConfig 
+
+
+    } // End Class IPtables
+
+
+} // End Namespace NiHaoCookie
