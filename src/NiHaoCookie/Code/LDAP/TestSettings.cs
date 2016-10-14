5fb5ca0c8a4f2cb747ac006523c87d1e12f974a9
Administrator <quandary82@hailmail.net>
2016-10-14 14:47:08 +0200

    Added Novell.LDAP for AD Authentication

---
+++ src/NiHaoCookie/Code/LDAP/TestSettings.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NiHaoCookie.Samples
+{
+    class TestSettings
+    {
+
+        public static string ldapHost = "servername.somedomain.local"; // args[0];
+        public static int ldapPort = 389;//System.Convert.ToInt32(args[1]);
+        public static string loginDN = @"domain\user"; // args[2];
+        public static string password = @"TopSecret"; // args[3];
+
+    }
+}
